c87772e40b6dee074882dc46e9b16406a1a95aa8
jordan123803 <100330489+jordan123803@users.noreply.github.com>
2022-02-28 17:12:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <font color="blue"><font size = "6"><b>First homework for php:</b></font></font>
+        <br/>
+        <br/>
+    </head>
+    <body>
+        <?php
+        $i=10;
+        $j=100;
+        echo "<b>Hello</b> <i>DinDin</i> <u>love u</u>";
+        echo "<br/>";
+        echo "i+j = ";
+        echo $i+$j;
+        ?>
+    </body>
+</html>
